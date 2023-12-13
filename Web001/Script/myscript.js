aa71f2e853fea6bb799754e8184ec933d9c2f25f
MefedronDestroyer <151124942+MefedronDestroyer@users.noreply.github.com>
2023-12-13 11:30:31 +0300

    Add files via upload

---
+++ Web001/Script/myscript.js
@@ -0,0 +1,11 @@
+var btn = document.getElementById('MyBtn')
+var modal = document.getElementById('MyModal')
+var close = document.getElementById('myClouse')
+ function  red()
+{
+modal.style.display = "block";
+}
+close.onclick = function()
+{
+modal.style.display = "none";
+}
